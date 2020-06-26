ca28cdf3148567ac8a0ef896ce31792e724f9712
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-25 20:07:24 -0400

    Make create date optional as not all SFSs will have this

--- app/src/main/java/com/vandenbreemen/mobilesecurestorage/file/api/FileInfo.kt
+++ app/src/main/java/com/vandenbreemen/mobilesecurestorage/file/api/FileInfo.kt
@@ -6,4 +6,4 @@ import java.util.*
  * Information about a file stored on the SFS
  * @author kevin
  */
-data class FileInfo(val fileName: String, val size: Int, val createDate: Calendar)
+data class FileInfo(val fileName: String, val size: Int, val createDate: Calendar?)
